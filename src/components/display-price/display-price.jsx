2c3ccf051b65d3e0a2f982ad239c25b3bba72876
Aurélien <aurelien.strimelle@bstorm.be>
2022-01-27 14:52:30 +0100

    Add comment for Intl

--- src/components/display-price/display-price.jsx
+++ src/components/display-price/display-price.jsx
@@ -2,6 +2,9 @@ import PropTypes from 'prop-types';
 
 const DisplayPrice = (props) => {
 
+    // Utilisation de "Intl" pour formatté la valeur numerique
+    //   en une valeur "devise" pour la belgique.
+    // https://developer.mozilla.org/fr/docs/Web/JavaScript/Reference/Global_Objects/Intl/NumberFormat
     const price = new Intl.NumberFormat('fr-BE', { style: 'currency', currency: 'EUR' })
                           .format(props.value)
 
